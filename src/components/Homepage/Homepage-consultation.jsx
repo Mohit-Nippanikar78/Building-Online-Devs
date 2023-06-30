a4592d1b752dbb37c0a23f741682958d3918a134
Mohit <rnippanikar7@gmail.com>
2023-06-30 21:08:47 +0530

    Updated Homepage About Consultation

---
+++ src/components/Homepage/Homepage-consultation.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import CustomButton from '../elements/CustomButton'
+import { useNavigate } from 'react-router-dom'
+
+const HomepageConsultation = () => {
+    let navigate = useNavigate()
+    return (
+        <div className="flex max-h-[320px]  justify-evenly items-scretch my-32 h-full ">
+            <img
+                src="./images/homepage/about-2.png"
+                className="object-none"
+                alt=""
+            />
+            <div
+                className="flex  justify-center px-4 rounded-md  flex-col text-white max-w-lg  bg-[#F1F1F110] px-6 py-8  h-full "
+                style={{
+                    boxShadow: "rgba(241, 241, 241,.7) 0px 0px 2px 0px",
+                }}
+            >
+                <div className="text-3xl font-bold capitalize ">
+                    we can offer
+                    <span className="text-lime"> personal </span>
+                    help
+                </div>
+                <div className="font-normal  py-6 text-sm">
+                    <div>
+                        Lorem ipsum dolor sit amet consectetur. Elit potenti urna ac
+                        vulputate. Enim purus sit nibh sed quis dignissim mattis congue
+                        et. Massa cursus morbi viverra tellus augue arcu.
+                    </div>
+                    <div className="mt-4">
+                        sed in turpis pretium eu maecenas. Lobortis sit nec nisl ornare
+                        mus eget. Egestas odio aliquam tempor dictum lorem id mauris sit
+                        ultrices. Ullamcorper eu cursus a elit. Quam urna.
+                    </div>
+                </div>
+                <CustomButton text="Get personal consultation" func={()=>{navigate("/")}} />
+            </div>
+        </div>
+    )
+}
+
+export default HomepageConsultation
